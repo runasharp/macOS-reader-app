b2d9efd694ab35e3b0827226747b3b3e9281aa36
runasharp <43112986+runasharp@users.noreply.github.com>
2025-01-05 19:40:50 +0100

    Initial Commit

---
+++ MacReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacReader
+//
+//  Created by Anastasia Ivanova on 05.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacReader/MacReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacReaderApp.swift
+//  MacReader
+//
+//  Created by Anastasia Ivanova on 05.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MacReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
