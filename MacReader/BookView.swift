85bf910f44a4ce0355be749ac4aab15fd6f251aa
runasharp <43112986+runasharp@users.noreply.github.com>
2025-01-13 20:30:41 +0100

    Added a book view

---
+++ MacReader/BookView.swift
@@ -0,0 +1,18 @@
+//
+//  BookView.swift
+//  MacReader
+//
+//  Created by Anastasia Ivanova on 13.01.25.
+//
+
+import SwiftUI
+
+struct BookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookView()
+}
